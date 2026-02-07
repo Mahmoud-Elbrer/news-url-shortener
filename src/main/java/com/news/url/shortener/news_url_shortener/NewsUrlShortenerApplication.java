21b340a65d48dae1a327b6c2a2eb88ac80cf8dd6
Mahmoud-Elbrer <111395110+Mahmoud-Elbrer@users.noreply.github.com>
2026-02-08 02:04:43 +0400

    first commit

---
+++ src/main/java/com/news/url/shortener/news_url_shortener/NewsUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.news.url.shortener.news_url_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsUrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsUrlShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/news/url/shortener/news_url_shortener/NewsUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news.url.shortener.news_url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
